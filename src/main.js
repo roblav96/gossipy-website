c9a8bf3fa69a3b22a00be4c06b254f19aaad5718
roblav96 <roblav96@gmx.com>
2016-11-09 01:38:06 -0500

    dirp

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
+// 
+
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+
+
+
+Vue.use(VueRouter)
+
+
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+	el: '#app',
+	render: h => h(App)
 })
+
